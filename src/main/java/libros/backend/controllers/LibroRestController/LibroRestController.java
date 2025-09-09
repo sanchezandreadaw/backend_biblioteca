45116a571546ea2badae4ab4ce7d0a50c8cd53c6
sanchezandreadaw <sanchezmoraandreadaw@gmail.com>
2025-09-09 13:07:54 +0200

    creacion de ficheros para controladores

---
+++ src/main/java/libros/backend/controllers/LibroRestController/LibroRestController.java
@@ -0,0 +1,10 @@
+package libros.backend.controllers.LibroRestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/libros")
+public class LibroRestController {
+
+}
---
+++ src/main/java/libros/backend/controllers/UserRestController/UserRestController.java
@@ -0,0 +1,10 @@
+package libros.backend.controllers.UserRestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+
+}
